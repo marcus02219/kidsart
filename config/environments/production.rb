0aafc513fae95a210745867cd9e21528681ca7da
marcus <marcus02219@gmail.com>
2016-05-18 23:21:31 +0300

    modify secret token

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  ENV["SECRET_KEY_BASE"] = "9dd1cef234a2758fc76d3d0f9c2291a824dc74d49ec9caafeb8b7d9e3dadd13b2c59272dfabe8cf753b3e33ee813d268bd843bcde07635e075769f2e8f50608d"
 end
