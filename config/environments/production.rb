8b486373aa3747bd92535e381fe6cab815e0e826
marcus <marcus02219@gmail.com>
2016-06-25 07:47:12 +0300

    modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "198.58.105.103",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "huawan028@gmail.com",
