0d9d6c84f397e380971218fba3dd5de2ec9466bd
marcus <marcus02219@gmail.com>
2016-06-28 05:05:22 +0300

    modify

--- app/api/endpoints/accounts.rb
+++ app/api/endpoints/accounts.rb
@@ -24,19 +24,16 @@ module Endpoints
             :secret_access_key => ENV['AMAZON_SECRET_KEY'],
           )
           verified_emails = ses.addresses.list.result
+
           if verified_emails.include? user.email
-            ses.send_email(
-               :to        => [user.email],
-               :source    => '"KidsArt" <noreply@kidsart.media>',
-               :subject   => 'Forgot Password',
-               :text_body => 'Internal text body'
-            )
+            user.send_reset_password_instructions
+            {success: 'Email was sent successfully'}
           else
             ses.addresses.verify(user.email)
+            {failure: 'Please check your email.'}
           end
 
-          user.send_reset_password_instructions
-          {success: 'Email was sent successfully'}
+
         else
           {:failure => 'Cannot find your email'}
         end
